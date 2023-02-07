564bb50b9e78fa05be2c3a8976ecf0c336cbe527
dmitkate <44602492+dmitkate@users.noreply.github.com>
2023-02-07 18:19:46 +0200

    дожано app.use(express.static('public'));

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const usersRouter = require('./routes/api/user')
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
+app.use(express.static('public'));
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
